ebcd01cb7bda3df3ec2dc4615191511963038d4a
allahverdia <165601933+allahverdia@users.noreply.github.com>
2024-10-21 18:59:00 -0400

    Update updater.rs

--- interface/code/src/updater.rs
+++ interface/code/src/updater.rs
@@ -6,22 +6,21 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
-
-//use std::io::{self};
 use std::process::Command;
-//use std::path::Path;
-//use std::io::stdout;
-//use std::str;
-//use bstr::ByteSlice;
 
-
-pub fn upload_program_ext(filepath: String, port_name: String, baud_rate: String){
-
+pub fn upload_program_ext(filepath: String, port_name: String, baud_rate: String) {
     let filearg: String = "-Uflash:w:".to_owned() + &filepath;
     let port_name_full: String = "-P".to_owned() + &port_name;
     let baud_rate_full: String = "-b".to_owned() + &baud_rate;
 
-   let _output = Command::new("avrdude").arg("-v").arg("-patmega328p").arg("-carduino").arg(&port_name_full).arg(&baud_rate_full).arg("-D").arg(&filearg).output().expect("Upload Failed");
-
+    let _output = Command::new("avrdude")
+        .arg("-v")
+        .arg("-patmega328p")
+        .arg("-carduino")
+        .arg(&port_name_full)
+        .arg(&baud_rate_full)
+        .arg("-D")
+        .arg(&filearg)
+        .output()
+        .expect("Upload Failed");
 }
-
