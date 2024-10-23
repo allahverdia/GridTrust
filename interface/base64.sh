73efbe2529f219ca8225cf67e39ba2b3f24369e4
allahverdia <165601933+allahverdia@users.noreply.github.com>
2024-10-23 12:17:33 -0400

    Delete interface/base64.sh

--- interface/base64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-base64 -d /updater/files_to_use/util_sig.txt > /updater/files_to_use/util_sig_conv.txt
-base64 -d /updater/files_to_use/vendor_sig.txt > /updater/files_to_use/vendor_sig_conv.txt
-base64 -d /updater/files_to_use/update64.txt > /updater/files_to_use/update.txt
